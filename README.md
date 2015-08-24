9bc92f0eb6b0440d6b191d5847aa931d13d9706b
luobotang <tangkangxing@sina.com>
2015-08-24 12:37:28 +0800

    v0.1.1 refactor

--- README.md
+++ README.md
@@ -2,12 +2,53 @@
 
 JavaScript，将 CSS 内容以添加 style 节点方式应用到页面。
 
+## 安装
+
+```hash
+npm install luobotang-addstyle
+```
+
 ## 用法
 
-```JavaScript
+```javascript
 AddStyle('p { color: red; }')
 AddStyle({p: {color: 'blue'}})
 ```
+
+CSS 规则可以通过多种方式传入：
+
+1、规则文本
+
+例如：
+```
+'p { color: red; } strong { font-weight: bold; }'
+```
+
+2、规则对象
+
+例如：
+```javascript
+{
+    p: {
+        color: 'red'
+    },
+    strong: 'font-weight: bold'
+}
+```
+
+3、规则数组
+
+例如：
+```javascript
+[
+    ['p', 'color: red;'],
+    ['strong', {
+        'font-weight': 'bold'
+    }]
+]
+```
+
+注：如示例中所示，CSS 规则声明支持文本或对象形式。
 
 ## 兼容性
 
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 (function (factory) {
 
 	if (typeof module === 'objects' && module.exports) {
-		module.exports = factory();
+		module.exports = factory()
 	} else {
-		window.AddStyle = factory();
+		window.AddStyle = factory()
 	}
 
 })(function () {
+
+var isArray = Array.isArray ? function (o) {
+	return Array.isArray(o)
+} : function (o) {
+	return Object.prototype.toString.call(o) === '[object Array]'
+}
 
 /*
  * 向页面添加 css 样式
@@ -17,58 +23,97 @@
  *   // 样式数据对象
  *   addStyle({p: { color: 'red' }})
  *
- * @param {string|Object} css - 要添加的 css 文本，或者可以转为 css 文本的样式数据对象
+ * @param {string|Object|Array} css - 要添加的 css 文本，或者可以转为 css 文本的样式数据对象
  * @returns {HTMLStyleElement}
  */
-function addStyle(css) {
-    var ref = document.getElementsByTagName('script')[0],
-        eleStyle = document.createElement('style');
+function addStyle(rules) {
+	var ele = document.createElement('style')
+	ele.type = "text/css"
 
-	if (typeof css === 'object') {
-		css = renderStyles(css);
+	var cssText = renderRules(rules)
+
+	if (ele.styleSheet) { // IE
+		ele.styleSheet.cssText = cssText
+	} else {
+		ele.appendChild(
+			document.createTextNode(cssText)
+		)
 	}
 
-    eleStyle.type = "text/css";
-    if (eleStyle.styleSheet) { // IE
-        eleStyle.styleSheet.cssText = css;
-    } else {
-        eleStyle.appendChild(
-        	document.createTextNode(css)
-        );
-    }
-
-    ref.parentNode.insertBefore(eleStyle, ref);
-    return eleStyle;
+	// 将新增的 style 节点插入第一个 script 标签之前
+	var ref = document.getElementsByTagName('script')[0]
+	ref.parentNode.insertBefore(ele, ref)
+	return ele;
 }
 
 /*
  * 样式数据对象转为 css 文本
- * @param {Object} styles
+ * @param {Object|Array} rules
  * @returns {string}
  */
-function renderStyles(styles) {
-    var styleHtml = '',
-        selector,
-        propHtml,
-        properties, propName, propVal,
-        toStr = Object.prototype.toString;
+function renderRules(rules) {
+    var cssText = ''
 
-    for (selector in styles) {
-        properties = styles[selector];
-        props = [];
-        for (propName in properties) {
-            propVal = properties[propName];
-            if (toStr.call(propVal) === '[object Array]') {
-                props.push(propName + ':' + propVal.join(' '));
-            } else {
-                props.push(propName + ':' + propVal);
-            }
-        }
-        styleHtml = selector + '{' + props.join(';') + '}';
-    }
+	if (!rules) return cssText
+	if (typeof rules === 'string') return rules
 
-    return styleHtml;
+	/*
+	 * Rule struct as Array
+	 * [
+	 *   [<Selector>, <Declaration>],
+	 *   ...
+	 * ]
+	 */
+	if (isArray(rules)) {
+		var rule
+		var i, len
+		for (i = 0, len === rules.length; i < len; i++) {
+			rule = rules[i]
+			cssText += rule2css(rule[0], rule[1])
+		}
+	}
+	/*
+	 * Rule struct as Object
+	 * {
+	 *   'Selector': <Declaration>,
+	 *   ...
+	 * }
+	 */
+	else {
+		var selector
+		for (selector in rules) {
+			cssText += rule2css(selector, rules[selector])
+		}
+	}
+
+    return cssText;
 }
 
-return addStyle;
-});
+// CSSRule
+// <CSSRule> =>
+//   <selector> { <declaration> }
+// <declaration> =>
+//   property: value;
+//   property: value;
+//   ...
+
+function rule2css(selector, declaration) {
+	if (typeof declaration === 'object') {
+		var ret = []
+		var property
+		var value
+		for (property in declaration) {
+			value = declaration[property]
+			ret.push(
+				property + ':' + (
+					isArray(value) ? value.join(' ') : value
+				)
+			)
+		}
+		declaration = ret.join(';')
+	}
+	return selector + '{' + declaration + '}'
+}
+
+return addStyle
+})
